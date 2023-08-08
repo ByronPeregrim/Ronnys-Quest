7f4d1a6db43ea62861913f78d8fe3b382c619a3e
Byron Peregrim <byronperegrim@gmail.com>
2023-08-07 22:30:32 -0400

    Imported tilesheet and images. Added terrain and other static images to display surface.

--- code/level.py
+++ code/level.py
@@ -1,17 +1,34 @@
 import pygame
-from support import import_csv_layout
+from support import import_csv_layout, import_cut_graphics
 from settings import tile_size
-from tiles import Tile
+from tiles import Tile, StaticTile, Grass, Box, Bush, Tree
 
 class Level:
     def __init__(self,level_data,surface):
         # general setup
         self.display_surface = surface
-        self.world_shift = 0
+        self.world_shift = -5
 
         # terrain setup
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout,'terrain')
+
+        # grass setup
+        grass_layout = import_csv_layout(level_data['grass'])
+        self.grass_sprites = self.create_tile_group(grass_layout,'grass')
+
+        # box setup
+        box_layout = import_csv_layout(level_data['boxes'])
+        self.box_sprites = self.create_tile_group(box_layout,'boxes')
+
+        # bush setup
+        bush_layout = import_csv_layout(level_data['bushes'])
+        self.bush_sprites = self.create_tile_group(bush_layout,'bushes')
+
+        # tree setup
+        tree_layout = import_csv_layout(level_data['trees'])
+        self.tree_sprites = self.create_tile_group(tree_layout,'trees')
+
 
     def create_tile_group(self,layout,type):
         sprite_group = pygame.sprite.Group()
@@ -23,13 +40,48 @@ class Level:
                     y = row_index * tile_size
 
                     if type == 'terrain':
-                        sprite = Tile(tile_size,x,y)
-                        sprite_group.add(sprite)
+                        terrain_tile_list = import_cut_graphics('../graphics/terrains/Tileset.png')
+                        tile_surface = terrain_tile_list[int(val)]
+                        sprite = StaticTile(tile_size,x,y,tile_surface)
+                    
+                    if type == 'grass':
+                        sprite = Grass(tile_size,x,y,val)
 
+                    if type == 'boxes':
+                        sprite = Box(tile_size,x,y,val)
+
+                    if type == 'bushes':
+                        sprite = Bush(tile_size,x,y,val)
+
+                    if type == 'trees':
+                        sprite = Tree(tile_size,x,y,val)
+
+                    sprite_group.add(sprite)
+
+                    
 
         return sprite_group
 
     def run(self):
         # run the entire level
-        self.terrain_sprites.draw(self.display_surface)
+
+        # terrain
         self.terrain_sprites.update(self.world_shift)
+        self.terrain_sprites.draw(self.display_surface)
+        
+        # grass
+        self.grass_sprites.update(self.world_shift)
+        self.grass_sprites.draw(self.display_surface)
+
+        # boxes
+        self.box_sprites.update(self.world_shift)
+        self.box_sprites.draw(self.display_surface)
+
+        # bushes
+        self.bush_sprites.update(self.world_shift)
+        self.bush_sprites.draw(self.display_surface)
+
+        # trees
+        self.tree_sprites.update(self.world_shift)
+        self.tree_sprites.draw(self.display_surface)
+        
--- code/tiles.py
+++ code/tiles.py
@@ -9,3 +9,32 @@ class Tile(pygame.sprite.Sprite):
 
     def update(self,shift):
         self.rect.x += shift
+
+class StaticTile(Tile):
+    def __init__(self,size,x,y,surface):
+        super().__init__(size,x,y)
+        self.image = surface 
+
+class Grass(StaticTile):
+    def __init__(self,size,x,y,val):
+        super().__init__(size,x,y,pygame.image.load(f'../graphics/decoration/Grass/{int(val)+1}.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x,offset_y))
+
+class Box(StaticTile):
+    def __init__(self,size,x,y,val):
+        super().__init__(size,x,y,pygame.image.load(f'../graphics/decoration/Boxes/{int(val)+1}.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x,offset_y))
+
+class Bush(StaticTile):
+    def __init__(self,size,x,y,val):
+        super().__init__(size,x,y,pygame.image.load(f'../graphics/decoration/Bushes/{int(val)+1}.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x,offset_y))
+
+class Tree(StaticTile):
+    def __init__(self,size,x,y,val):
+        super().__init__(size,x,y,pygame.image.load(f'../graphics/decoration/Trees/{int(val)+1}.png').convert_alpha())
+        offset_y = y + size
+        self.rect = self.image.get_rect(bottomleft = (x,offset_y))
